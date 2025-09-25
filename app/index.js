9b37089c98544c5090cc0b4981098fb428d67185
Nagisa Flowers <nagisa.ryuko93@gmail.com>
2025-09-24 23:49:20 -0600

    Aplicacion y pipeline CI

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('¡Hola desde CI/CD!');
+});
+app.listen(port,()=>{
+    console.log('App escuchandoen http://localhost:${port}');
+});
